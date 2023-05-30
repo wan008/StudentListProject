5b25ac2f8f2ab61d8c8d37a2ce8ed28812d1aaaf
wan008 <kimw@sheridancollege.ca>
2023-05-30 05:58:05 -0400

    Added AddPartTime.java file

---
+++ src/studentlist/AddPartTime.java
@@ -0,0 +1,16 @@
+
+package studentlist;
+
+
+public class AddPartTime {
+    
+    String name;
+    String studentId;
+
+    public AddPartTime(String name, String studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+    
+    
+}
